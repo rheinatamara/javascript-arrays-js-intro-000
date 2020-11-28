248cd125677c3cb1d0820e0f8aa4318cc70a4131
Rheina <rheina.tamara@outlook.com>
2020-11-28 17:10:11 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array,element){
+
+  return [element,...array];
+}
+console.log(addElementToBeginningOfArray(chocolateBars, "mars") );
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element);
+  return array;
+}
+console.log(destructivelyAddElementToBeginningOfArray(chocolateBars,"mars"));
+
+function addElementToEndOfArray(array, element){
+
+  return [...array,element];
+}
+console.log(addElementToEndOfArray(chocolateBars, "mars") );
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array;
+}
+console.log(destructivelyAddElementToEndOfArray(chocolateBars,"mars"));
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+console.log(accessElementInArray(chocolateBars,0));
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+console.log(destructivelyRemoveElementFromBeginningOfArray(chocolateBars));
+
+function removeElementFromBeginningOfArray(array){
+  array.filter(function (n,index){
+    return index !== 0;
+  })
+  return array;
+}
+console.log(removeElementFromBeginningOfArray(chocolateBars));
